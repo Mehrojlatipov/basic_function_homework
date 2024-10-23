ba9d0482b6706d1b71758ef19f192a1b4100e299
Mehrojlatipov <mehrojlatifov06@gmail.com>
2024-10-24 01:08:01 +0500

    done_08

--- basic_func08.py
+++ basic_func08.py
@@ -1,7 +1,7 @@
 # Create a function called main.
 def main(a):
 # Create function arguments a.
-    a = a * a
+    a = a + 1
 # Increase the value of a to one and return.
     return a
 print (main(12))
